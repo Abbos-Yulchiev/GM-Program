8a7ee46496f52ef23c8abbc4751ba34b5be61ab3
Abbos Yulchiev <Abbos_Yulchiev@epam.com>
2022-11-10 10:21:59 +0500

    spring data access model added

---
+++ spring-data-access/spring-data-access/src/main/java/com/study/springdataaccess/SpringDataAccessApplication.java
@@ -0,0 +1,13 @@
+package com.study.springdataaccess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataAccessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataAccessApplication.class, args);
+	}
+
+}
---
+++ spring-data-access/spring-data-access/src/test/java/com/study/springdataaccess/SpringDataAccessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.springdataaccess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataAccessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
